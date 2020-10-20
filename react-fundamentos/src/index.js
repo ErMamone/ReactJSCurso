1ab283452e951256365365cedf72baeba44f2108
Eros Mamone <46795580+ErMamone@users.noreply.github.com>
2020-10-19 21:06:45 -0300

    Clase 18

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,1 +1,36 @@
-alert('Hallooooo');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class TarjetaFruta extends React.Component{
+    constructor(){
+        super()
+
+        this.state ={
+            cantidad : 5
+        }
+    }
+
+    render(){
+        return(
+        < div >
+            <h3>{this.props.name}</h3>
+            <div>Cantidad: {this.state.cantidad}</div>
+            <button onClick={ () => {
+                this.setState({ cantidad: this.state.cantidad +1})
+            }}>Agregar</button>
+            
+            <hr/>
+            <p>$ {this.props.price}</p>
+        </div>)
+    }
+}
+
+const App = () => (
+    < div>
+        <TarjetaFruta name = {'Sandia'} price={2.00}/>
+        <TarjetaFruta name = {'Kiwi'} price={1.00}/>
+        <TarjetaFruta name = {`naranja`} price={5.00}/>
+    </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
